b8f9e987ef3cdb2782ef8e0655f705845056e6b7
leormix <82319032+leormix@users.noreply.github.com>
2025-11-18 21:06:22 +0200

    fixes

--- lab5/array.js
+++ lab5/array.js
@@ -1,28 +1,25 @@
 function array() {
-    let data = []
-    function arr(method, ...args) {
-        switch (method) {
-            case "push":
-                return data.push(...args);
-            case "pop":
-                return data.pop();
-            case "get":
-                return args.length ? data[args[0]] : data;
+    let data = [];
+
+    function arr(i) {
+        if (i === undefined) {
+            return data
         }
+        return data[i];
     }
+
     arr.push = (...args) => data.push(...args);
     arr.pop = () => data.pop();
-    arr.get = (i) => (i === undefined ? data : data[i]);
 
     return arr;
 }
 
-const a = array()
+const a = array();
 
-a.push("home")
-a.push("joke")
-a.push("markus")
-a.pop()
+a.push("home");
+a.push("joke");
+a.push("markus");
+a.pop();
 
-console.log(a.get(0))
-console.log(a.get())
+console.log(a(0));
+console.log(a()); 
