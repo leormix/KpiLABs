1e411a617d8879fb9af5f43cd1ea931b17bd92ed
leormix <82319032+leormix@users.noreply.github.com>
2025-11-25 15:51:21 +0200

    fixes

--- lab7/unique.js
+++ lab7/unique.js
@@ -1,10 +1,10 @@
 'use strict'
-    
+
 const array = [1, 2, 2, 2, 3, 5, 5, 5, 6]
 
-function unique(array) {
+function unique(list) {
     const result = []
-    for (item of array) {
+    for (const item of list) {
         if (!result.includes(item)) {
             result.push(item)
         }
